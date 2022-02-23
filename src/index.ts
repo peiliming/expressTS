0cbbc7c4a626c0e5e7c1f89c8531907ad6879503
 みんちゃん <ming@minchannoMacBook-Pro-2.local>
2022-02-23 19:52:35 +0900

    nodemonでファイル更新時にtypescriptを自動的にコンパイル

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,8 @@ const users: User[] = [
     { id: 1, name: "User1", email: "user1@test.local" },
     { id: 2, name: "User2", email: "user2@test.local" },
     { id: 3, name: "User3", email: "user3@test.local" },
-    { id: 4, name: "User4", email: "user4@test.local" }
+    { id: 4, name: "User4", email: "user4@test.local" },
+    { id: 5, name: "User5", email: "user4@test.local" }
 ]
 
 //一覧取得
